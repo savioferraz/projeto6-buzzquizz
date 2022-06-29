f1a566d2ffcee271efd167e79940583f45024c39
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-06-29 14:52:08 -0300

    colocando as imagens na tela

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ pegaQuizesDoServidor();
 function quizesDoServidorNaTela(todosQuizes){
     const i = todosQuizes;
    for (let i = 0; i < todosQuizes.data.length; i++) {
-        document.querySelector(".seus-quizes .com-quiz").innerHTML +=` <div class="imagens"><img src="${todosQuizes.data[i].image}"><h2 class="legenda">${todosQuizes.data[i].title}</h2></div>`;
+        document.querySelector(".todos-os-quizes .com-quiz").innerHTML +=` <div class="imagens"><img src="${todosQuizes.data[i].image}"><h2 class="legenda">${todosQuizes.data[i].title}</h2></div>`;
     }
    }
 
