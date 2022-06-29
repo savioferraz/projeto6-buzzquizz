84cdd97db99dcb04b920ebd913842c3beb4bc94a
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-06-29 19:51:49 -0300

    colocando as perguntas

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,34 @@
+const quizes = [];
+
 verificaSeTemQuiz();
 
 pegaQuizesDoServidor();
 
-//function topoPaginaInicial(){ 
+
+function quizesDoServidorNaTela(todosQuizes) {
+    quizes.push(todosQuizes);
+    topoPaginaInicial();
+   for (let i = 0; i < todosQuizes.data.length; i++) {
+        document.querySelector(".todos-os-quizes .quizes").innerHTML +=` <div class="imagens" onclick="selecionaQuiz(this,${i})" ><img src="${todosQuizes.data[i].image}"><h2 class="legenda">${todosQuizes.data[i].title}</h2></div>`;
+    }
+}
+
+function pegaQuizesDoServidor(){
+   const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promessa.then(quizesDoServidorNaTela);
+}
+
+function topoPaginaInicial(){ 
     document.querySelector(".conteudo").innerHTML+=`
     <div class="todos-os-quizes">
         <h1 class="titulo">Todos os quizes</h1>
         <div class="quizes">
         </div>
     </div>`;
-
-//}
-
-function quizesDoServidorNaTela(todosQuizes) {
-    topoPaginaInicial();
-   for (let i = 0; i < todosQuizes.data.length; i++) {
-        document.querySelector(".todos-os-quizes .quizes").innerHTML +=` <div class="imagens"><img src="${todosQuizes.data[i].image}"><h2 class="legenda">${todosQuizes.data[i].title}</h2></div>`;
-    }
-   }
-
-
-function pegaQuizesDoServidor(){
-   const promessa = axios.get("https://mock-api.driven.com.br/api/v3/buzzquizz/quizzes");
-    promessa.then(quizesDoServidorNaTela);
 }
 
-//function verificaSeTemQuiz(){
-    //aqui tera a condição que verifica se a mais quizes desse usuario como ainda não a sempre ira mostrrar atela inicial//
-    if(false){
+function baixoPaginainicial(retorno){
+    if(retorno === false){
         document.querySelector(".conteudo").innerHTML =`
         <h1 class="titulo primeiro">Seus quizes</h1>
         <div class="quizes-usuario">
@@ -42,8 +43,36 @@ function pegaQuizesDoServidor(){
         <button onclick="paginaDoQuestionario(this)" >Criar quizz</button>
     </div> 
 </div>`;
-//}
+}
 
+function verificaSeTemQuiz(){
+    //aqui tera a condição que verifica se a mais quizes desse usuario como ainda não a sempre ira mostrrar atela inicial//
+    baixoPaginainicial(true);   
+}
 function criarQuiz(){
 
 }
+
+function selecionaQuiz(elemento, numeroElemento){
+    let objeto = quizes[0].data[numeroElemento];
+    colocaTelaDoquiz(elemento);
+    colocaAsPerguntas(objeto);
+}
+
+function colocaTelaDoquiz(elemento){
+    document.querySelector(".conteudo").innerHTML =` <div class="titulo-quiz">  <div class="imagem-titulo"><img src="${elemento.firstChild.src}"> </div> <div class="titulo-imagem">${elemento.textContent}</div></div><div class="caixa-perguntas">  </div>`;  
+}
+
+function colocaAsPerguntas(objeto){
+    console.log(objeto);
+    for (let i = 0; i < objeto.levels.length; i++) {
+        document.querySelector(".caixa-perguntas").innerHTML +=`<div class="titulo-pergunta">${objeto.levels[i].title} </div><div class="respostas"></div> `;
+       for (let e = 0; e < objeto.questions.length; e++) {
+        document.querySelector(".respostas").innerHTML +=` <div class="resposta-legenda"> "${objeto.levels[e].title} </div><div class="resposta-imagem"><img src="${objeto.levels[e].image}"></div></div></div></div>;`;
+   
+       }
+    }
+    
+}
+
+
