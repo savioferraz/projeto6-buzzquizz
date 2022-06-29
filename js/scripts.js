f6d4aba1bcc2582db093797a438e50344cd499d1
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-06-29 09:30:01 -0300

    tentativa de colocar as imagens dos jogos na pagina

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
-verificaSeTemQuiz()
+verificaSeTemQuiz();
+
+pegaQuizesDoServidor();
+
+
+function quizesDoServidorNaTela(todosQuizes){
+    console.log(todosQuizes.data[0]);
+    document.querySelector(".todos-os-quizes ul").innerHTML =`<a href="${todosQuizes.data[0].image}" src=""></a>`;
+}
+
+function pegaQuizesDoServidor(){
+   const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    promessa.then(quizesDoServidorNaTela);
+}
 
 function verificaSeTemQuiz(){
     //aqui tera a condição que verifica se a mais quizes desse usuario como ainda não a sempre ira mostrrar atela inicial//
@@ -7,4 +20,8 @@ function verificaSeTemQuiz(){
     <p>Você não criou nenhum <br> quizz ainda :(</p>
     <h2 onclick="criarQuiz()">Criar Quizz</h2>
 </div>`;
+}
+
+function criarQuiz(){
+
 }
