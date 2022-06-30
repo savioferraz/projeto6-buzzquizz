a579ed8ccc486b302eae5f009ee4ec0702dc0424
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-06-30 20:06:33 -0300

    terminando funçoes do jogo

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ function quizesDoServidorNaTela(todosQuizes) {
 }
 
 function pegaQuizesDoServidor(){
-   const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+   const promessa = axios.get("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes");
     promessa.then(quizesDoServidorNaTela);
 }
 
@@ -71,9 +71,7 @@ function colocaTelaDoquiz(){
 function colocaAsPerguntas(){
     for (let i = 0; i < objeto[0].questions.length; i++) {
         let caixaDePergunta = document.querySelector(".conteudo");
-        caixaDePergunta.innerHTML +=`<div class="caixa-perguntas"> <div class="titulo-pergunta">${objeto[0].questions[i].title} </div><div class="respostas"></div></div> `;
-        
-        console.log(caixaDePergunta.children);
+        caixaDePergunta.innerHTML +=`<div class="caixa-perguntas"> <div class="titulo-pergunta" style="background-color:${objeto[0].questions[i].color} ;" >${objeto[0].questions[i].title} </div><div class="respostas"></div></div> `;
         for (let e = 0; e < objeto[0].questions[i].answers.length ; e++) {
             caixaDePergunta.children[i+1].querySelector(".respostas").innerHTML +=`<div class="resposta-imagem"><img onclick="confereAcerto(this,${i},${e})" src="${objeto[0].questions[i].answers[e].image}"><div class="resposta-legenda">${objeto[0].questions[i].answers[e].text} </div></div>`;
         }
@@ -81,6 +79,12 @@ function colocaAsPerguntas(){
 }
 
 function confereAcerto(elemento,numerolistaClicado,numeroElementoClicado){  
+    let todasCaixas =document.querySelector(".conteudo").children;
+    if (numerolistaClicado+2 !== todasCaixas.length ) {
+        todasCaixas[numerolistaClicado+2].querySelector(".respostas").scrollIntoView({block: "end"});    
+    }
+    
+    //.querySelector(".respostas").scrollIntoView();
     let listaDeImagens = elemento.parentNode.parentNode;
     totalJogadas++;
     if (objeto[0].questions[numerolistaClicado].answers[numeroElementoClicado].isCorrectAnswer) {
@@ -88,14 +92,52 @@ function confereAcerto(elemento,numerolistaClicado,numeroElementoClicado){
     }
     for (let i = 0; i < objeto[0].questions[numerolistaClicado].answers.length; i++) {
         if (objeto[0].questions[numerolistaClicado].answers[numeroElementoClicado] !== objeto[0].questions[numerolistaClicado].answers[i]) {
-            console.log(listaDeImagens);
             listaDeImagens.children[i].querySelector("img").classList.add("opaco");
             listaDeImagens.children[i].querySelector("img").removeAttribute("onclick");
         }
         listaDeImagens.children[i].querySelector("img").removeAttribute("onclick");
         
     }
+    if(objeto[0].questions.length===totalJogadas){
+        finalizaQuiz();
+    }
+        
+}
+
+function finalizaQuiz(){
+    const porcentagem = (totalAcertos*100)/(objeto[0].questions.length)
+    for (let i = 0; i < objeto[0].levels.length; i++) {
+        if( porcentagem >= objeto[0].levels[i].minValue ){
+            document.querySelector(".conteudo").innerHTML +=`<div class="caixa-perguntas"> <div class="titulo-pergunta" >${porcentagem.toFixed(0)}% de acerto: ${objeto[0].levels[i].title} </div><div class="respostas"><div class="resposta-imagem"><img src="${objeto[0].levels[i].image}"><div class="resposta-legenda"> ${objeto[0].levels[i].text}</div></div></div></div> <div class="botão-reinicia" onclick="reiniciarQuiz()" >Reiniciar Quizz</div><div class="botão-volta-home" onclick="voltaHome()" >Voltar pra home</div>`;
+            document.querySelector(".conteudo").lastChild.scrollIntoView()
+            return;
+        }
+    }
+        for (let i = 0; i < objeto[0].levels.length; i++) {
+        if( porcentagem <= objeto[0].levels[i].minValue ){
+            document.querySelector(".conteudo").innerHTML +=`<div class="caixa-perguntas"> <div class="titulo-pergunta" >${porcentagem.toFixed(0)}% de acerto: ${objeto[0].levels[i].title} </div><div class="respostas"><div class="resposta-imagem"><img src="${objeto[0].levels[i].image}"><div class="resposta-legenda">${objeto[0].levels[i].text} </div></div></div></div> `;
+            document.querySelector(".conteudo").lastChild.scrollIntoView();
+            return;
+        }    
+       
+    }
+
     
 }
-// cixa a div.respostas
-// cixaDePergunta tem que apontar respostas
+
+function reiniciarQuiz(){
+    totalAcertos=0;
+    totalJogadas=0;    
+    colocaTelaDoquiz();
+    colocaAsPerguntas();
+    document.querySelector(".titulo-quiz").scrollIntoView();
+
+}
+
+function voltaHome() {
+    window.location.reload();
+}
+
+// const ultimaMensagem = document.querySelector(".xx:last-child");
+//     if(ultimaMensagem !== null){
+//         ultimaMensagem.scrollIntoView();
