6c2ee61581ca928ea4093d3c2694fb6e0899293b
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-06-30 14:18:32 -0300

    terminando funçoes do jogo

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ function quizesDoServidorNaTela(todosQuizes) {
 }
 
 function pegaQuizesDoServidor(){
-   const promessa = axios.get("https://mock-api.driven.com.br/api/v3/buzzquizz/quizzes");
+   const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
     promessa.then(quizesDoServidorNaTela);
 }
 
@@ -65,20 +65,18 @@ function selecionaQuiz(numeroElemento){
 }
 
 function colocaTelaDoquiz(){
-    document.querySelector(".conteudo").innerHTML =` <div class="titulo-quiz">  <div class="imagem-titulo"><img src="${objeto[0].image}"> </div> <div class="titulo-imagem">${objeto[0].title}</div></div><div class="caixa-perguntas">  </div>`;  
-}
-
-function colocaTelaDoquiz(){
-    document.querySelector(".conteudo").innerHTML =` <div class="titulo-quiz">  <div class="imagem-titulo"><img src="${objeto[0].image}"> </div> <div class="titulo-imagem">${objeto[0].title}</div></div><div class="caixa-perguntas">  </div>`;  
+    document.querySelector(".conteudo").innerHTML =` <div class="titulo-quiz">  <div class="imagem-titulo"><img src="${objeto[0].image}"> </div> <div class="titulo-imagem">${objeto[0].title}</div></div>`;  
 }
 
 function colocaAsPerguntas(){
     for (let i = 0; i < objeto[0].questions.length; i++) {
-        let caixaDePergunta = document.querySelector(".caixa-perguntas");
-        caixaDePergunta.innerHTML +=`<div class="titulo-pergunta">${objeto[0].questions[i].title} </div><div class="respostas"></div> `;
+        let caixaDePergunta = document.querySelector(".conteudo");
+        caixaDePergunta.innerHTML +=`<div class="caixa-perguntas"> <div class="titulo-pergunta">${objeto[0].questions[i].title} </div><div class="respostas"></div></div> `;
+        
+        console.log(caixaDePergunta.children);
         for (let e = 0; e < objeto[0].questions[i].answers.length ; e++) {
-            caixaDePergunta.lastElementChild.innerHTML +=`<div class="resposta-imagem"><img onclick="confereAcerto(this,${i},${e})" src="${objeto[0].questions[i].answers[e].image}"><div class="resposta-legenda">${objeto[0].questions[i].answers[e].text} </div></div>`;
-       }
+            caixaDePergunta.children[i+1].querySelector(".respostas").innerHTML +=`<div class="resposta-imagem"><img onclick="confereAcerto(this,${i},${e})" src="${objeto[0].questions[i].answers[e].image}"><div class="resposta-legenda">${objeto[0].questions[i].answers[e].text} </div></div>`;
+        }
     }
 }
 
@@ -90,11 +88,14 @@ function confereAcerto(elemento,numerolistaClicado,numeroElementoClicado){
     }
     for (let i = 0; i < objeto[0].questions[numerolistaClicado].answers.length; i++) {
         if (objeto[0].questions[numerolistaClicado].answers[numeroElementoClicado] !== objeto[0].questions[numerolistaClicado].answers[i]) {
+            console.log(listaDeImagens);
             listaDeImagens.children[i].querySelector("img").classList.add("opaco");
             listaDeImagens.children[i].querySelector("img").removeAttribute("onclick");
         }
         listaDeImagens.children[i].querySelector("img").removeAttribute("onclick");
         
     }
-    console.log(totalAcertos,totalJogadas);
+    
 }
+// cixa a div.respostas
+// cixaDePergunta tem que apontar respostas
