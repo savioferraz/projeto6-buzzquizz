dae7ab5f09442f8a99496d344fe4d329b10ebebd
savioferrazgit config --global user.name savioferrazclearclsgit config --global user.name savioferrazgit config --global user.name savioferraz <savio.ferraz88@gmail.com>
2022-06-30 21:27:31 -0300

    layout desktop finalizado

--- js/scripts.js
+++ js/scripts.js
@@ -19,17 +19,10 @@
 //     }
 // }
 
-<<<<<<< HEAD
-// function pegaQuizesDoServidor(){
-//    const promessa = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
-//     promessa.then(quizesDoServidorNaTela);
-// }
-=======
 function pegaQuizesDoServidor(){
    const promessa = axios.get("https://mock-api.driven.com.br/api/v7/buzzquizz/quizzes");
     promessa.then(quizesDoServidorNaTela);
 }
->>>>>>> a579ed8ccc486b302eae5f009ee4ec0702dc0424
 
 // function topoPaginaInicial(){ 
 //     document.querySelector(".conteudo").innerHTML+=`
@@ -75,39 +68,6 @@ function pegaQuizesDoServidor(){
 //     document.querySelector(".conteudo").innerHTML =` <div class="titulo-quiz">  <div class="imagem-titulo"><img src="${objeto[0].image}"> </div> <div class="titulo-imagem">${objeto[0].title}</div></div>`;  
 // }
 
-<<<<<<< HEAD
-// function colocaAsPerguntas(){
-//     for (let i = 0; i < objeto[0].questions.length; i++) {
-//         let caixaDePergunta = document.querySelector(".conteudo");
-//         caixaDePergunta.innerHTML +=`<div class="caixa-perguntas"> <div class="titulo-pergunta">${objeto[0].questions[i].title} </div><div class="respostas"></div></div> `;
-        
-//         console.log(caixaDePergunta.children);
-//         for (let e = 0; e < objeto[0].questions[i].answers.length ; e++) {
-//             caixaDePergunta.children[i+1].querySelector(".respostas").innerHTML +=`<div class="resposta-imagem"><img onclick="confereAcerto(this,${i},${e})" src="${objeto[0].questions[i].answers[e].image}"><div class="resposta-legenda">${objeto[0].questions[i].answers[e].text} </div></div>`;
-//         }
-//     }
-// }
-
-// function confereAcerto(elemento,numerolistaClicado,numeroElementoClicado){  
-//     let listaDeImagens = elemento.parentNode.parentNode;
-//     totalJogadas++;
-//     if (objeto[0].questions[numerolistaClicado].answers[numeroElementoClicado].isCorrectAnswer) {
-//         totalAcertos++;
-//     }
-//     for (let i = 0; i < objeto[0].questions[numerolistaClicado].answers.length; i++) {
-//         if (objeto[0].questions[numerolistaClicado].answers[numeroElementoClicado] !== objeto[0].questions[numerolistaClicado].answers[i]) {
-//             console.log(listaDeImagens);
-//             listaDeImagens.children[i].querySelector("img").classList.add("opaco");
-//             listaDeImagens.children[i].querySelector("img").removeAttribute("onclick");
-//         }
-//         listaDeImagens.children[i].querySelector("img").removeAttribute("onclick");
-        
-//     }
-    
-// }
-// cixa a div.respostas
-// cixaDePergunta tem que apontar respostas
-=======
 function colocaAsPerguntas(){
     for (let i = 0; i < objeto[0].questions.length; i++) {
         let caixaDePergunta = document.querySelector(".conteudo");
@@ -181,4 +141,3 @@ function voltaHome() {
 // const ultimaMensagem = document.querySelector(".xx:last-child");
 //     if(ultimaMensagem !== null){
 //         ultimaMensagem.scrollIntoView();
->>>>>>> a579ed8ccc486b302eae5f009ee4ec0702dc0424
